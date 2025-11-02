5ed08a67303c3c19643f15b7396cf2300db62f78
Éléonore Hasler <116505700+e-hasler@users.noreply.github.com>
2025-11-02 11:45:36 +0000

    fix dates format

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,13 @@ import datetime
 
 st.title("🌍 My Travel Journal")
 
-# Load entries from JSON
 with open("data.json", "r") as f:
     entries = json.load(f)
 
-# Date picker
-date = st.date_input("Pick a date", min_value="2000-01-01")
+date = st.date_input("Pick a date", min_value="01-01-2000",format="DD/MM/YYYY")
 
-# Convert to string format
 date_str = date.strftime("%Y-%m-%d")
 
-# Display
 if date_str in entries:
     st.subheader(f"📅 {date.strftime('%B %d, %Y')}")
     st.write(entries[date_str])
