23793d6be968f4e2dbe58528b9b2afd2c8d2705e
Éléonore Hasler <116505700+e-hasler@users.noreply.github.com>
2025-11-02 12:25:39 +0000

    add periods of time in data

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,22 @@ st.title("🌍 My Travel Journal")
 with open("data.json", "r") as f:
     entries = json.load(f)
 
-date = st.date_input("Pick a date", min_value="01-01-2000",format="DD/MM/YYYY")
+date = st.date_input("Pick a date", min_value=datetime.date(2000,1,1), format="DD/MM/YYYY")
 
-date_str = date.strftime("%Y-%m-%d")
+date_str = date.strftime("%d-%m-%Y")
 
 if date_str in entries:
-    st.subheader(f"📅 {date.strftime('%B %d, %Y')}")
+    st.subheader(f"📅 {date.strftime('%d %B %Y')}")
     st.write(entries[date_str])
+elif date_str[3:] in entries:
+    st.subheader(f"📅 {date.strftime('%d %B %Y')}")
+    st.write(entries[date_str[3:]])
+elif date_str[6:] in entries:
+    st.subheader(f"📅 {date.strftime('%d %B %Y')}")
+    st.write(entries[date_str[6:]])
 else:
-    st.subheader(f"📅 {date.strftime('%B %d, %Y')}")
+    st.subheader(f"📅 {date.strftime('%d %B %Y')}")
     st.info("Nothing")
+
+
+
